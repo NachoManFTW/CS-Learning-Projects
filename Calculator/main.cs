a1618e1a735695d7bc07ddce4128426d9a3d3df8
Daniel Pererva <156557819+NachoManFTW@users.noreply.github.com>
2025-08-21 15:43:37 -0500

    Finished Program
    Swapped from integers to doubles for the decimal points if they want to use those

--- Calculator/main.cs
+++ Calculator/main.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
+//Main Namespace
 namespace Calculator
 {
+    //Main Program Class
     class Program
     {
-
-
+        //Main Function
         static int Main()
         {
-            int num1 = 0;
-            int num2 = 0;
-            int operationSelector;
+            double num1;
+            double num2;
+            int operationSelector = 0;
 
             string[] operations = { "1.Add", "2.Subtract", "3.Multiply", "4.Divide" };
             //Program name
@@ -23,84 +20,94 @@ namespace Calculator
 
             while (true)
             {
-                //First number input
-                Console.Write("Enter First Number: ");
-                try
+                while (true)
                 {
-                    num1 = Convert.ToInt16(Console.ReadLine());
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Invaild Input");
-                }
-            }
-
-            while (true)
-            { 
-                //Second number input
-                Console.Write("Enter Second Number: ");
-                try
-                {
-                    num2 = Convert.ToInt16(Console.ReadLine());
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Invaild Input");
-                }
-            }
-
-            //List all operations
-            foreach (string operation in operations)
-            {
-                Console.WriteLine(operation);
-            }
-
-            //Get operation type
-            while (true)
-            {
-                Console.Write("Please select one by entering the corresponding number: ");
-                try
-                {
-                    operationSelector = Convert.ToUInt16(Console.ReadLine()) - 1;
-                    if (operationSelector > 3)
+                    //First number input
+                    Console.Write("Enter First Number: ");
+                    try
                     {
-                        throw new Exception();
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        break;
                     }
-                    if (operationSelector == 3 && num2 == 0)
+                    catch (Exception)
                     {
-                        throw new Exception();
+                        Console.WriteLine("Invaild Input");
                     }
-                    break;
                 }
-                catch (Exception ex)
+
+                while (true)
                 {
-                    Console.WriteLine("Invalid Input");
-                    Console.WriteLine("Input out of bounds");
+                    //Second number input
+                    Console.Write("Enter Second Number: ");
+                    try
+                    {
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invaild Input");
+                    }
+                }
+
+                //List all operations
+                foreach (string operation in operations)
+                {
+                    Console.WriteLine(operation);
+                }
+
+                //Get operation type
+                while (true)
+                {
+                    Console.Write("Please select one by entering the corresponding number: ");
+                    try
+                    {
+                        operationSelector = Convert.ToUInt16(Console.ReadLine()) - 1;
+                        if (operationSelector > 3 || operationSelector == 3 && num2 == 0)
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        if (operationSelector == 3 && num2 == 0)
+                            Console.WriteLine("Cannot divide by zero");
+                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("Input out of bounds");
+                    }
+
+
+                }
+
+                //Go to operation
+                switch (operationSelector)
+                {
+                    case 0:
+                        Console.WriteLine(Add(num1, num2));
+                        break;
+                    case 1:
+                        Console.WriteLine(Subtract(num1, num2));
+                        break;
+                    case 2:
+                        Console.WriteLine(Multiply(num1, num2));
+                        break;
+                    case 3:
+                        Console.WriteLine(Divide(num1, num2));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection, please try again!");
+                        break;
                 }
 
 
-            }
+                // Loop request 
+                Console.WriteLine("Press q to exit");
+                Console.WriteLine("Press space to continue \n\n");
 
-            //Go to operation
-            switch (operationSelector)
-            {
-                case 0:
-                    Console.WriteLine(add(num1, num2));
+                if ('q' == Console.ReadKey(true).KeyChar)
                     break;
-                case 1:
-                    Console.WriteLine(subtract(num1, num2));
-                    break;
-                case 2:
-                    Console.WriteLine(multiply(num1, num2));
-                    break;
-                case 3:
-                    Console.WriteLine(divide(num1, num2));
-                    break;
-                default:
-                    Console.WriteLine("Invalid selection, please try again!");
-                    break;
+                Console.Clear();
             }
     
 
@@ -108,25 +115,17 @@ namespace Calculator
             return 0;
         }
 
-        static int add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
+        // Add 2 numbers: retunrs sum
+        static double Add(double num1, double num2){return num1 + num2;}
 
-        static int subtract(int num1, int num2)
-        {
-            return num1 - num2;
-        }
+        //Subtract 2 numbers: returns remainder
+        static double Subtract(double num1, double num2){return num1 - num2;}
 
-        static int multiply(int num1, int num2)
-        {
-            return num1 * num2;
-        }
+        //Multiply 2 numbers: returns product
+        static double Multiply(double num1, double num2){return num1 * num2;}
 
-        static int divide(int num1, int num2)
-        {
-            return num1 / num2;
-        }
+        //Divide 2 numbers: returns dividen
+        static double Divide(double num1, double num2){return num1 / num2;}
 
     }
 }
