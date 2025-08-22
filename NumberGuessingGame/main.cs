95a77d22673bd287405338b1b295d72abd5cae93
Daniel Pererva <156557819+NachoManFTW@users.noreply.github.com>
2025-08-22 12:50:07 -0500

    Added a random number game
    Guess the random number and win the game, basic conditionals and loops

---
+++ NumberGuessingGame/main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int randomNumber = 0;
+            int guess = 0;
+            RandomNumberGenerator(ref randomNumber);
+            Console.WriteLine("Random Number Generator");
+            Console.WriteLine("Guess a number between 0 and 20");
+
+            while (true)
+            {
+                Console.Write("\n\nYour Guess: ");
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("ERROR: Invalid Input");
+                }
+
+                HigherOrLower(ref randomNumber, ref guess);
+
+
+                if (guess == randomNumber)
+                    break;
+            }
+            Console.WriteLine("You Won!!!");
+            Console.WriteLine("Exiting...");
+        }
+    
+        static void RandomNumberGenerator(ref int randomNumber)
+        {
+            Random random = new Random();
+            randomNumber = random.Next(21);
+        }
+
+        static void HigherOrLower(ref int randomNumber, ref int guess)
+        {
+            if (randomNumber > guess)
+                Console.WriteLine("Higher");
+            else if (randomNumber < guess)
+                Console.WriteLine("Lower");
+        }
+    }
+}
