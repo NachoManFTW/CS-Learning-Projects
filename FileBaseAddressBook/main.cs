31e4cb60c244fc7925d89a67c06acba8937cb972
Daniel Pererva <156557819+NachoManFTW@users.noreply.github.com>
2025-08-22 16:06:35 -0500

    Started New Small Project File Based Address Book
    Create a new small project that outputs to a file and input from a file to edit from

---
+++ FileBaseAddressBook/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileBaseAddressBook
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
